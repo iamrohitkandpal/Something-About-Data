d4c4c55b03bdba5f12b3c83458b81357edff3d1c
Rohit Kandpal <iamrohitkandpal@gmail.com>
2025-10-08 23:56:07 +0530

    Removed files for some file and folder structuring

--- src/models/product.py
+++
@@ -1,9 +0,0 @@
-class Product:
-
-    def __init__(self, name, price, category):
-        self.name = name
-        self.price = price
-        self.category = category
-
-    def __str__(self):
-        return f"Product: {self.name}, ${self.price}, {self.category}"
